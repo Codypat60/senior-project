c5bf14afe5daec3aab7737860b9e14016054ed31
Codypat60 <pattersoncody19@gmail.com>
2023-11-13 20:19:25 -0500

    Added node, cors, express, & calls

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-import axios from 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
 
-axios.get('https://aviationweather.gov/api/data/metar?ids=KHUF')
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/data/metar', async (req, res) => {
+  try {
+    const response = await axios.get('https://aviationweather.gov/api/data/metar', {
+      params: req.query,
     });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
